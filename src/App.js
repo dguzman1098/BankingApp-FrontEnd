10739ef3f22ca73a2f7e34784f3892c0336da402
Salisaak <Salisaak@gmail.com>
2021-12-01 13:40:29 -0500

    com

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+// eslint-disable-next-line 
+import { Navbar, Container, Nav } from 'react-bootstrap';
+// eslint-disable-next-line 
+import { Route, Routes } from 'react-router-dom'; 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component { 
+   constructor(props){
+       super(props);
+       this.state = {
+       
+       }
+ }
+
+render() { 
+  return ( 
+
+      <div className="App">
+            
+           <header>
+          </header>
+
+          <main>
+          
+          <Routes>
+          </Routes>
+
+          </main>
+
+
+      </div>
+    )
+  }
+
+}
+
+
+
+export default App
